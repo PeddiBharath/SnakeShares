72de2e9a75ff3786e10c3a0c9659cf29d0be0378
Peddi Bharath <21211a05n1@bvrit.ac.in>
2024-08-03 13:11:15 +0530

    add calendar

---
+++ pages/2_calendar.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Calendar")
+st.markdown(
+    """
+    <iframe src="https://v2-embednotion.com/099913472f7341aca944d727c7265ffc" style="width: 100%; height: 500px; border: 2px solid #ccc; border-radius: 10px; padding: none;"></iframe>
+    """,
+    unsafe_allow_html=True
+)
